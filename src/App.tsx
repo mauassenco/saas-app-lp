83320059c5a0555351420aa7cc69b05a537e30ab
mauassenco <mau.assenco@gmail.com>
2024-09-25 16:20:41 -0300

    chore: add Prettier Tailwind plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from "react"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="absolute flex items-end justify-end space-x-2 text-clip leading-3 text-red-500">
+      App
+      <button className="rounded bg-blue-500 px-4 py-2 text-base text-white">
+        Oi
+      </button>
+    </div>
   )
 }
 
