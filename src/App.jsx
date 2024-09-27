852664b281c79d62edee411b458a7f2f2dacf1b7
mauassenco <mau.assenco@gmail.com>
2024-09-27 18:11:48 -0300

    feat: creating Pricing component section - wip

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import Header from "./sections/Header.jsx"
 import Hero from "./sections/Hero.jsx"
 import Features from "./sections/Features.jsx"
+import Pricing from "./sections/Pricing.jsx"
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
       <Features />
+      <Pricing />
     </main>
   )
 }
